a6208b92dd95e1f9c9a496e976c856316dfcbcd0
sandra877 <mimolovescoffee@gmail.com>
2025-11-11 15:55:39 +0300

    bug

--- src/Pages/ContactUsPage.tsx
+++ src/Pages/ContactUsPage.tsx
@@ -1,5 +1,5 @@
 // src/Pages/ContactPage.tsx
-import ContactIntro from "../components/contactus/contactintro";
+import ContactIntro from "../components/contactus/ContactInro";
 import ContactForm from "../components/contactus/ContactForm";
 
 export const ContactPage = () => {
